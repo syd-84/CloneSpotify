690749548b52d9b24aa3f6900e9257fd79ae353c
syd84 <sydoruk84@gmail.com>
2026-04-06 23:04:27 +0300

    maked module makeAlbum.js for drawing list of Album

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 import { playlist } from "./fetchesResults/getPlaylist.js";
 import { myTopTracks } from "./fetchesResults/myTracks.js";
+import { albumTracks } from "./fetchesResults/albumTracks.js";
+import { album } from "./fetchesResults/getAlbum.js";
 
 import { fetchWebApi } from "./request.js"
 import { makeFullPlaylist } from "./makePlaylist.js";
 import { makeFullUserTopTracks } from "./myTopTracks.js";
-
+import { makeFullAlbum } from "./makeAlbum.js";
 
 // async function topTracksCurrentUser() {
 //   return (await fetchWebApi(
 //     `https://api.spotify.com/v1/me/top/tracks?limit=50`, 'GET'
 //   ));
 // }
-// let myTopTracks = await topTracksCurrentUser();
-
 
 
 // async function getPlaylist(id) {
@@ -20,18 +20,41 @@ import { makeFullUserTopTracks } from "./myTopTracks.js";
 //     `https://api.spotify.com/v1/playlists/${id}`, 'GET'
 //   ));
 // }
+
+
+// async function getAlbum(id) {
+//   return (await fetchWebApi(
+//     `https://api.spotify.com/v1/albums/${id}`, 'GET'
+//   ));
+// }
+
+
+// async function getAlbumList(id) {
+//   return (await fetchWebApi(
+//     `https://api.spotify.com/v1/albums/${id}/tracks?market=UA&limit=50`, 'GET'
+//   ));
+// }
+
+
+// let myTopTracks = await topTracksCurrentUser();
 // let playlist = await getPlaylist("2iZTFETkt7Qr6tbETaJDh4");
+// let album = await getAlbum("6tG8sCK4htJOLjlWwb7gZB");
+// let albumTracks = await getAlbumList("6tG8sCK4htJOLjlWwb7gZB");
+
+
+
 
 // makeFullPlaylist(playlist);
-
-
+// makeFullAlbum(albumTracks, album);
 
 makeFullUserTopTracks(myTopTracks);
+
+
 
 document.getElementById('logo').addEventListener('click', () => {
   makeFullUserTopTracks(myTopTracks);
 })
 
 document.getElementById('central_side').addEventListener('click', () => {
-  makeFullPlaylist(playlist);
+  makeFullAlbum(albumTracks, album);
 })
---
+++ js/makeAlbum.js
@@ -0,0 +1,62 @@
+export { makeFullAlbum };
+import { getArtists, msToTimeFormat } from "./helper.js";
+
+function makeAlbumItem(item, index) {
+  let divItem = document.createElement('div');
+  divItem.classList.add('list_item', `${item.uri.replaceAll(':', '_')}`);
+  divItem.innerHTML = `
+                <div class="list_number">
+                  <p>${index + 1}</p>
+                  <div class="artist_play_btn">
+                    <svg width="24" height="24">
+                      <use href="./images/icons.svg#play_pl"></use>
+                    </svg>
+                  </div>
+                </div>
+                <div class="list_data">
+                  <div>
+                    <div class="list_name">${item.name}</div>
+                    <div class="list_artist">${getArtists(item.artists)}</div>
+                  </div>
+                  <div class="list_last">${msToTimeFormat(item.duration_ms)}</div>
+                </div>
+  `;
+  document.getElementsByClassName('list')[0].append(divItem);
+}
+
+function makeAlbumList(albumList) {
+  albumList.items.forEach((element, index) => {
+    makeAlbumItem(element, index);
+  });
+}
+
+function makeFullAlbum(albumTracks, album) {
+  document.getElementById('start_section').innerHTML = `
+          <div id="album_envelope">
+            <div>
+              <img src="${album.images[0].url}" alt="">
+            </div>
+            <div>
+              <p>Альбом</p>
+              <h2>${album.name}</h2>
+            </div>
+          </div>
+
+          <div id="album_tracks" class="spotify_album_6tG8sCK4htJOLjlWwb7gZB">
+            <div class="list">
+              <div class="head_tracks">
+                <div class="list_number">#</div>
+                <div class="head_name">Назва пісні</div>
+                <div class="last_col">
+                  <svg width="24" height="24">
+                    <use href="./images/icons.svg#duration"></use>
+                  </svg>
+                </div>
+              </div>
+              <hr>
+
+            </div>
+          </div>
+  `;
+  makeAlbumList(albumTracks)
+}
